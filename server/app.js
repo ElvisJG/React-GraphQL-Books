6ed6aa60db54f6a1a3bd4ce8f917cb401057e420
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-06-29 10:25:47 -0400

    graphQL boilerplate work

--- server/app.js
+++ server/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const scheme = require('./schema/schema');
+const schema = require('./schema/schema');
 
 const app = express();
 
 app.use(
   '/graphql',
   graphqlHTTP({
-    schema
+    schema,
+    graphiql: true
   })
 );
 
